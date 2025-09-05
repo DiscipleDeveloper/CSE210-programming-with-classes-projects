aefaf51f4fb7596c60f7bf173b8d626b2a979b02
DiscipleDeveloper <ntumale@byupathway.edu>
2025-09-05 23:07:45 +0800

    camelCase is standard practice in BYU for C#

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -5,11 +5,14 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise1 Project.");
+
         Console.Write("What is your first name? ");
-        string FirstName = Console.ReadLine();
+        string firstName = Console.ReadLine();
+
         Console.Write("What is your last name? ");
-        string LastName = Console.ReadLine();
-        Console.WriteLine($"Your name is {LastName}, {FirstName} {LastName}.");
+        string lastName = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
     }
 }
 

af952733a814929a0735adda21ed95c0bca85764
DiscipleDeveloper <ntumale@byupathway.edu>
2025-09-06 17:12:26 +0800

    while loops in C#

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,38 @@
 using System;
+using System.Security.Cryptography;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
+
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        
+        int magicGuess = -1;
+
+
+        while (magicGuess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            string mGuess = Console.ReadLine();
+            magicGuess = int.Parse(mGuess);
+
+            if (magicGuess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (magicGuess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+
+        }
     }
 }
